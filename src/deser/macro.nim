fbd512f4bf76526a7d43019a79e84930901e2331
Gabben <43146729+gabbhack@users.noreply.github.com>
2022-06-09 01:12:16 +0500

    Yes

--- src/deser/macro.nim
+++ src/deser/macro.nim
@@ -1,9 +1,11 @@
 {.experimental: "strictFuncs".}
-import std/[macros, options, sugar]
+import std/[macros, options, tables]
 
 import pragmas
 
 template suka() {.pragma.}
+
+template suka1(a: int, b: string) {.pragma.}
 
 type
   ObjectTy = object
@@ -52,7 +54,7 @@ type
     fields: seq[Field]
 
   Test = ref object
-    id {.suka, suka.}: int
+    id {.suka1(1, "123").}: int
     lol: string
 
     case kind: bool
@@ -62,6 +64,22 @@ type
       b: int
   
   Alias = Test
+
+proc fill(self: var FieldFeatures, sym: NimNode, values: seq[NimNode] = @[]) =
+  self.untagged = sym == bindSym("untagged")
+  self.skipped = sym == bindSym("skipped")
+  self.skipSerializing = sym == bindSym("skipSerializing")
+  self.skipDeserializing = sym == bindSym("skipDeserializing")
+  self.inlineKeys = sym == bindSym("inlineKeys")
+
+  if sym == bindSym("serializeWith"):
+    self.serializeWith = some values[0]
+  elif sym == bindSym("renameSerialize"):
+    self.renameSerialize = some values[0].strVal
+  elif sym == bindSym("renameDeserialize"):
+    self.renameDeserialize = some values[0].strVal
+  elif sym == bindSym("skipSerializeIf"):
+    self.skipSerializeIf = some values[0]
 
 func by(T: typedesc[ObjectTy], sym: NimNode): T =
   expectKind sym, nnkSym
@@ -84,10 +102,21 @@ func by(T: typedesc[ObjectTy], sym: NimNode): T =
     expectKind typeImpl, {nnkSym, nnkRefTy, nnkObjectTy}
 
 
-func by(T: typedesc[FieldFeatures], pragma: NimNode): T =
-  expectKind pragma, nnkPragma
-  # TODO
-  discard
+func by(T: typedesc[FieldFeatures], pragmas: NimNode): T =
+  expectKind pragmas, nnkPragma
+  
+  for pragma in pragmas:
+    case pragma.kind
+    of nnkSym:
+      let sym = pragma
+      result.fill(sym)
+    of nnkCall:
+      # For {. pragmaName(something) .}
+      let sym = pragma[0]
+      let values = if pragma.len > 1: pragma[1..pragma.len-1] else: @[]
+      result.fill(sym, values)
+    else:
+      expectKind pragma, {nnkSym, nnkCall}
 
 
 func by(T: typedesc[Field], identDefs: NimNode): T =
@@ -102,6 +131,7 @@ func by(T: typedesc[Field], identDefs: NimNode): T =
     result = Field(ident: identNode, symType: sym)
   of nnkPragmaExpr:
     result = Field(ident: identNode[0], symType: sym)
+    result.features = FieldFeatures.by identNode[1]
   else:
     expectKind identNode, {nnkIdent, nnkPragmaExpr}
 
@@ -120,14 +150,17 @@ func by(T: typedesc[seq[Field]], recList: NimNode): T =
 
 
 func by(T: typedesc[Struct], objectTy: ObjectTy): T =
-  result = Struct(sym: objectTy.sym, isRef: objectTy.isRef)
-
   let recList = objectTy.node[2]
-  result.fields = seq[Field].by(recList=recList)
+  result = Struct(
+    sym: objectTy.sym,
+    isRef: objectTy.isRef,
+    fields: seq[Field].by(recList=recList)
+  )
 
 
 proc explore(node: NimNode) =
   let objectTy = ObjectTy.by(sym=node)
+  discard Struct.by objectTy
   echo objectTy.node.treeRepr
 
 
