c90975720705f37628a02f7178b1576c0c6f75d8
Gabben <github@sosus.org>
2022-06-09 20:52:36 +0500

    Some fix

--- src/deser/macroutils.nim
+++ src/deser/macroutils.nim
@@ -3,9 +3,6 @@ import std/[macros, options, tables]
 
 import pragmas
 
-template suka() {.pragma.}
-
-template suka1(a: int, b: string) {.pragma.}
 
 type
   ObjectTy = object
@@ -20,7 +17,6 @@ type
   
   Field = object
     ident: NimNode
-    symType: NimNode
     features: FieldFeatures
 
     case isCase: bool
@@ -53,27 +49,27 @@ type
       discard
     fields: seq[Field]
 
-  Test = object
-    id {.suka1(1, "123").}: int
-    lol: string
-
-    case kind: bool
-    of false..true:
-      a: int
-  
-  Alias = Test
-
 # forward decl
 func by(T: typedesc[seq[Field]], recList: NimNode): T
 
-proc fill(self: var FieldFeatures, sym: NimNode, values: seq[NimNode] = @[]) =
-  self.untagged = sym == bindSym("untagged")
-  self.skipped = sym == bindSym("skipped")
-  self.skipSerializing = sym == bindSym("skipSerializing")
-  self.skipDeserializing = sym == bindSym("skipDeserializing")
-  self.inlineKeys = sym == bindSym("inlineKeys")
 
-  if sym == bindSym("serializeWith"):
+func copyWithoutChild(copyOf: NimNode, idx = 0, n = 1): NimNode =
+  result = copy copyOf
+  result.del idx, n
+
+
+proc fill(self: var FieldFeatures, sym: NimNode, values: seq[NimNode] = @[]) =
+  if sym == bindSym("untagged"):
+    self.untagged = true
+  elif sym == bindSym("skipped"):
+    self.skipped = true
+  elif sym == bindSym("skipSerializing"):
+    self.skipSerializing = true
+  elif sym == bindSym("skipDeserializing"):
+    self.skipDeserializing = true
+  elif sym == bindSym("inlineKeys"):
+    self.inlineKeys = true
+  elif sym == bindSym("serializeWith"):
     self.serializeWith = some values[0]
   elif sym == bindSym("renameSerialize"):
     self.renameSerialize = some values[0].strVal
@@ -83,10 +79,14 @@ proc fill(self: var FieldFeatures, sym: NimNode, values: seq[NimNode] = @[]) =
     self.skipSerializeIf = some values[0]
 
 func by(T: typedesc[ObjectTy], sym: NimNode): T =
+  # Get temp `ObjectTy` from symbol of type
   expectKind sym, nnkSym
 
   let typeDef = sym.getImpl
-  debugEcho typeDef.treeRepr
+
+  if defined(debugObjectTy):
+    debugEcho typeDef.treeRepr
+
   expectKind typeDef, nnkTypeDef
 
   let typeImpl = typeDef[2]
@@ -95,25 +95,30 @@ func by(T: typedesc[ObjectTy], sym: NimNode): T =
   of nnkSym:
     # For alias types
     # e.g. type Foo = Bar
-    result = by(T, typeImpl)
+    # recursively calling ourselves
+    result = ObjectTy.by typeImpl
   of nnkRefTy:
+    # For type Foo = ref object
     result = ObjectTy(isRef: true, sym: sym, node: typeImpl[0])
   of nnkObjectTy:
+    # # For type Foo = object
     result = ObjectTy(isRef: false, sym: sym, node: typeImpl)
   else:
     expectKind typeImpl, {nnkSym, nnkRefTy, nnkObjectTy}
 
 
 func by(T: typedesc[FieldFeatures], pragmas: NimNode): T =
+  # Check whether the field contains our pragmas
   expectKind pragmas, nnkPragma
   
   for pragma in pragmas:
     case pragma.kind
     of nnkSym:
+      # {.pragmaName.}
       let sym = pragma
       result.fill(sym)
     of nnkCall:
-      # For {. pragmaName(something) .}
+      # {.pragmaName(values).}
       let
         sym = pragma[0]
         values = if pragma.len > 1: pragma[1..pragma.len-1] else: @[]
@@ -123,29 +128,28 @@ func by(T: typedesc[FieldFeatures], pragmas: NimNode): T =
 
 
 func by(T: typedesc[Field], identDefs: NimNode): T =
+  # Get field from usual statement
   expectKind identDefs, nnkIdentDefs
 
-  let
-    identNode = identDefs[0]
-    sym = identDefs[1]
-  expectKind sym, nnkSym
+  let identNode = identDefs[0]
 
   case identNode.kind
   of nnkIdent:
-    result = Field(ident: identNode, symType: sym)
+    result = Field(ident: identNode)
   of nnkPragmaExpr:
-    result = Field(ident: identNode[0], symType: sym)
+    result = Field(ident: identNode[0])
     result.features = FieldFeatures.by(pragmas=identNode[1])
   else:
     expectKind identNode, {nnkIdent, nnkPragmaExpr}
 
 
 func by(T: typedesc[Field], recCase: NimNode): T =
+  # Get field from case statement
   expectKind recCase, nnkRecCase
 
   let identDefs = recCase[0]
-  var field = Field.by(identDefs=identDefs)
-  field.isCase = true
+  result = Field.by(identDefs=identDefs)
+  result.isCase = true
 
   let branches = recCase[1..recCase.len-1]
 
@@ -153,12 +157,13 @@ func by(T: typedesc[Field], recCase: NimNode): T =
     case branch.kind
     of nnkOfBranch:
       let
-        # TODO condition must contain all nodes from nnkOfBranch before nnkRecList
-        condition = copy branch
-        recList = branch[1]
+        # for future code generation
+        # we need to safe ofBranch without body
+        condition = branch.copyWithoutChild(branch.len-1)
+        recList = branch[branch.len-1]
         fields = seq[Field].by(recList=recList)
 
-      field.branches.add FieldBranch(
+      result.branches.add FieldBranch(
         kind: Of,
         condition: condition,
         fields: fields
@@ -168,7 +173,7 @@ func by(T: typedesc[Field], recCase: NimNode): T =
         recList = branch[0]
         fields = seq[Field].by(recList=recList)
       
-      field.branches.add FieldBranch(
+      result.branches.add FieldBranch(
         kind: Else,
         fields: fields
       )
@@ -202,13 +207,6 @@ func by(T: typedesc[Struct], objectTy: ObjectTy): T =
   )
 
 
-proc explore(node: NimNode) =
+func explore*(node: NimNode): Struct =
   let objectTy = ObjectTy.by(sym=node)
-  discard Struct.by objectTy
-  echo objectTy.node.treeRepr
-
-
-macro run(typ: typed{`type`}) =
-  explore typ
-
-run Alias
+  result = Struct.by objectTy
